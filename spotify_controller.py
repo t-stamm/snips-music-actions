1519d6b2f376b172ac67308dca6f9fd50d9aab73
t-stamm <timo.stamm@gmx.de>
2018-12-04 22:43:09 +0100

    Working on spotify api authorization through console.

--- spotify_controller.py
+++ spotify_controller.py
@@ -8,11 +8,16 @@ class SpotifyController:
         self.token = ""
         #self.token = 'AQBJdX-hFLsJ-BZXAFR6xa6C7OlE01PogTQid5wsnaKxtfVUmvnbLAg--8OwRezchrWVVIh6dS8BOFEvyv3LWiO_0p5hs8Vvpqblp39forKAqPTRi2dbNwso3zwQKVIf_0OLBiNx4WxAZ9y3JphFPbhOwF1SZ70SfkZloInnD3zYubsYilqqDL1ihpx9Yt47rp8986-uGARGfj-kHWTJNYkK7wdguFPx5X1KiWkzHW6HKY_1CslOltpqS32ThR2XD71ozL_FE_wOK2Bo08OidEl7B_UrvSlvyOpXR4Bvfd8uMyOmloQn5mBZ8cuRxj1T1JGLDGa-WA1dxwVRfdv-oR1KsIxlEbDJmebDr0fHozu8WNWO41z1Jy0y-9L0gvbcGQ'
         if not self.token or self.token == "":
-            self.token = util.prompt_for_user_token('timo.stamm@gmx.de',
-                'user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state',
-                client_id='06aa17352eba440db3aa332ca605ae14', 
-                client_secret='f84b30beb6934037b44decd90c8a792b',
-                redirect_uri='http://localhost/')
+            #self.token = util.prompt_for_user_token('timo.stamm@gmx.de',
+            #    'user-library-read playlist-read-private playlist-read-collaborative user-modify-playback-state user-read-currently-playing user-read-playback-state',
+            #    client_id='06aa17352eba440db3aa332ca605ae14', 
+            #    client_secret='f84b30beb6934037b44decd90c8a792b',
+            #    redirect_uri='http://localhost/')
+
+            println "Please open the following url and paste the content from code parameter it returns: "
+            println "https://accounts.spotify.com/authorize?scope=playlist-read-collaborative+playlist-read-private+user-library-read+user-modify-playback-state+user-read-currently-playing+user-read-playback-state&redirect_uri=http%3A%2F%2Flocalhost%2F&response_type=code&client_id=06aa17352eba440db3aa332ca605ae14
+
+            self.token = input("Code: ")
 
         self.sp = spotipy.Spotify(auth=self.token)
     
